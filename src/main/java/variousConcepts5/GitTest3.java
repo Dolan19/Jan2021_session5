8d2c1c7a7a7473904c7427ae63bab8187641defc
Dolan19 <dolan.gbd@gmail.com>
2022-06-24 08:26:32 -0400

    git test 3

---
+++ src/main/java/variousConcepts5/GitTest3.java
@@ -0,0 +1,18 @@
+package variousConcepts5;
+
+public class GitTest3 {
+		public static void main(String[] args) {
+			System.out.println("test");
+			System.out.println("test1");
+			System.out.println("test2");
+			
+			printme();
+			
+		}
+		
+		public static void printme() {
+			
+			
+			System.out.println("print me");
+		}
+}
